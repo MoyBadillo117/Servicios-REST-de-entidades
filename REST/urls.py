83918e00b90006359467d72d9d92c83d5540256e
MoyBadillo117 <A00834306@tec.mx>
2023-03-30 18:21:18 -0600

    Se añadieron los metodos REST para las partidas

--- REST/urls.py
+++ REST/urls.py
@@ -1,10 +1,12 @@
 from django.urls import include,path
 from rest_framework import routers
 from . import views 
-
+from .views import Partidas 
 urlpatterns = [
    path('get', views.getUsuarios),
    path('post', views.postUsuario),
    path('put/<int:pk>', views.putUsuario),
-   path('delete/<int:pk>', views.deleteUsuario),
+   path('delete/<int:pk>', views.deleteUsuario), 
+   path('partidas',  Partidas.as_view(),name='Partidas'),
+   path('partidas/<int:id>',  Partidas.as_view(),name='Partida'),
 ]
--- REST/views.py
+++ REST/views.py
@@ -10,6 +10,10 @@ import sqlite3
 import requests 
 from .models import usuarios, Partida_Jugadores
 from .serializers import usuariosSerializer, Partida_JugadoresSerializer
+from django.views import View
+from django.http import JsonResponse
+from django.utils.decorators import method_decorator
+import json
 # Create your views here.
 
 
@@ -45,3 +49,57 @@ def deleteUsuario(request, pk):
     user = usuarios.objects.get(id=pk)
     user.delete()
     return Response("Usuario eliminado")
+
+class Partidas(View):
+    @method_decorator(csrf_exempt)
+    def dispatch(self, request, *args, **kwargs):
+        return super().dispatch(request, *args, **kwargs)
+
+    def get(self, request, id=0):
+        if(id>0):
+            partidas=partidas = list(Partida_Jugadores.objects.filter(id=id).values())
+            if len(partidas)>0:
+                partida=partidas[0]
+                datos = {'message': "Partida encontrada", 'partidas':partidas}
+            else:
+                datos = {'message':"No se encontró la partida"}
+            return JsonResponse(datos)
+        else:
+            partidas = list(Partida_Jugadores.objects.values()) #serializamos
+            if len(partidas)>0:
+                datos = {'message': "Succes", 'partidas':partidas}
+            else:
+                datos = {'message':"No se encontraron partidas"}
+            return JsonResponse(datos)
+    
+    def post(self,request):
+        jd = json.loads(request.body)
+        Partida_Jugadores.objects.create(fecha=jd['fecha'],id_usuario=usuarios.objects.get(pk = jd['id_usuario']),minutos_jugados=jd['minutos_jugados'],puntaje=jd['puntaje'])
+        datos = {'message': "Partida añadida"}
+        return JsonResponse(datos)
+    
+    def put(self, request,id):
+        jd = json.loads(request.body)
+        partidas=partidas = list(Partida_Jugadores.objects.filter(id=id).values())#Serializamos
+        if len(partidas)>0:
+            partidas=Partida_Jugadores.objects.get(id=id)
+            partidas.fecha= jd['fecha']
+            partidas.id_usuario= usuarios.objects.get(pk = jd['id_usuario'])
+            partidas.minutos_jugados= jd['minutos_jugados']
+            partidas.puntaje= jd['puntaje']
+            partidas.save()
+            datos = {'message':"Se modificó la partida"}
+        else:
+            datos = {'message':"No se encontró la pertida"}
+        return JsonResponse(datos)
+    
+    def delete(self, request,id):
+        partidas = list(Partida_Jugadores.objects.filter(id=id).values())
+        if len(partidas)>0:
+            Partida_Jugadores.objects.filter(id=id).delete()
+            datos = {'message':"Se eliminó la partida"}
+        else:
+            datos = {'message':"No se encontró la pertida"}
+        return JsonResponse(datos)
+
+        
