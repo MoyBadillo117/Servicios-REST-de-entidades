ef4eea9ea43b86a39bfcc3bf514ac519ff2b7618
MoyBadillo117 <A00834306@tec.mx>
2023-03-31 17:06:23 -0600

    Se modificó los servicios REST de usuario

--- REST/urls.py
+++ REST/urls.py
@@ -1,12 +1,9 @@
 from django.urls import include,path
-from rest_framework import routers
 from . import views 
-from .views import Partidas 
+from .views import Partidas, Usuarios
 urlpatterns = [
-   path('get', views.getUsuarios),
-   path('post', views.postUsuario),
-   path('put/<int:pk>', views.putUsuario),
-   path('delete/<int:pk>', views.deleteUsuario), 
    path('partidas',  Partidas.as_view(),name='Partidas'),
    path('partidas/<int:id>',  Partidas.as_view(),name='Partida'),
+   path('usuarios',  Usuarios.as_view(),name='Partidas'),
+   path('usuarios/<int:id>',  Usuarios.as_view(),name='Partida'),
 ]
--- REST/views.py
+++ REST/views.py
@@ -1,54 +1,60 @@
-from django.shortcuts import render
-from rest_framework import viewsets
-from rest_framework.response import Response 
-from rest_framework.decorators import api_view
-from django.http import Http404
-from django.http import HttpResponse
 from django.views.decorators.csrf import csrf_exempt
-from json import loads,dumps
-import sqlite3 
-import requests 
 from .models import usuarios, Partida_Jugadores
-from .serializers import usuariosSerializer, Partida_JugadoresSerializer
 from django.views import View
 from django.http import JsonResponse
 from django.utils.decorators import method_decorator
 import json
 # Create your views here.
 
+class Usuarios(View):
+    @method_decorator(csrf_exempt)
+    def dispatch(self, request, *args, **kwargs):
+        return super().dispatch(request, *args, **kwargs)
 
-@api_view(['GET'])
-@csrf_exempt
-def getUsuarios(request):
-    users = usuarios.objects.all()
-    serializer = usuariosSerializer(users, many=True)
-    return Response(serializer.data)
+    def get(self, request, id=0):
+        if(id>0):
+            users=list(usuarios.objects.filter(id=id).values())
+            if len(users)>0:
+                user=users[0]
+                datos = {'message': "Usuario encontrado", 'users':user}
+            else:
+                datos = {'message':"No se ha encontrado el usuario"}
+            return JsonResponse(datos)
+        else:
+            users = list(usuarios.objects.values()) #serializamos
+            if len(users)>0:
+                datos = {'message': "Succes", 'users':users}
+            else:
+                datos = {'message':"No se enconteraron usuarios"}
+            return JsonResponse(datos)
+    
+    def post(self,request):
+        jd = json.loads(request.body)
+        usuarios.objects.create(password=jd['password'])
+        datos = {'message': "Usuario añadido"}
+        return JsonResponse(datos)
+    
+    def put(self, request,id):
+        jd = json.loads(request.body)
+        users= list(usuarios.objects.filter(id=id).values())#Serializamos
+        if len(users)>0:
+            users=usuarios.objects.get(id=id)
+            users.password= jd['password']
+            users.save()
+            datos = {'message':"Se modificó la contraseña del usuario"}
+        else:
+            datos = {'message':"No se encontró el usuario"}
+        return JsonResponse(datos)
+    
+    def delete(self, request,id):
+        users = list(usuarios.objects.filter(id=id).values())
+        if len(users)>0:
+            usuarios.objects.filter(id=id).delete()
+            datos = {'message':"Se eliminó el usuario"}
+        else:
+            datos = {'message':"No se encontró el usuario"}
+        return JsonResponse(datos)
 
-
-@api_view(['POST'])
-@csrf_exempt
-def postUsuario(request):
-    data = request.data
-    user = usuarios.objects.create(
-        password = data['password']
-    ) 
-    serializer = usuariosSerializer(user, many=False)
-    return Response(serializer.data)
-
-@api_view(['PUT'])
-def putUsuario(request, pk):
-    data = request.data
-    user = usuarios.objects.get(id=pk)
-    serializer = usuariosSerializer(instance=user, data=data)
-    if serializer.is_valid():
-        serializer.save()
-    return Response(serializer.data)
-
-@api_view(['DELETE'])
-def deleteUsuario(request, pk):
-    user = usuarios.objects.get(id=pk)
-    user.delete()
-    return Response("Usuario eliminado")
 
 class Partidas(View):
     @method_decorator(csrf_exempt)
@@ -57,10 +63,10 @@ class Partidas(View):
 
     def get(self, request, id=0):
         if(id>0):
-            partidas=partidas = list(Partida_Jugadores.objects.filter(id=id).values())
+            partidas= list(Partida_Jugadores.objects.filter(id=id).values())
             if len(partidas)>0:
                 partida=partidas[0]
-                datos = {'message': "Partida encontrada", 'partidas':partidas}
+                datos = {'message': "Partida encontrada", 'partidas':partida}
             else:
                 datos = {'message':"No se encontró la partida"}
             return JsonResponse(datos)
@@ -80,7 +86,7 @@ class Partidas(View):
     
     def put(self, request,id):
         jd = json.loads(request.body)
-        partidas=partidas = list(Partida_Jugadores.objects.filter(id=id).values())#Serializamos
+        partidas= list(Partida_Jugadores.objects.filter(id=id).values())#Serializamos
         if len(partidas)>0:
             partidas=Partida_Jugadores.objects.get(id=id)
             partidas.fecha= jd['fecha']
@@ -90,7 +96,7 @@ class Partidas(View):
             partidas.save()
             datos = {'message':"Se modificó la partida"}
         else:
-            datos = {'message':"No se encontró la pertida"}
+            datos = {'message':"No se encontró la partida"}
         return JsonResponse(datos)
     
     def delete(self, request,id):
@@ -99,7 +105,6 @@ class Partidas(View):
             Partida_Jugadores.objects.filter(id=id).delete()
             datos = {'message':"Se eliminó la partida"}
         else:
-            datos = {'message':"No se encontró la pertida"}
+            datos = {'message':"No se encontró la partida"}
         return JsonResponse(datos)
 
-        
